426412ae902f941a6c11a55291ed5c4ed3372810
Sinan Özdemir <60213164+SinanOzdemir55@users.noreply.github.com>
2023-11-15 14:30:09 +0100

    db file

---
+++ db.js
@@ -0,0 +1,23 @@
+[
+    {
+      "id": 1,
+      "name": "John",
+      "surname": "Doe",
+      "email": "john.doe@example.com",
+      "tel": "555-1234"
+    },
+    {
+      "id": 2,
+      "name": "Jane",
+      "surname": "Smith",
+      "email": "jane.smith@example.com",
+      "tel": "555-5678"
+    },
+    {
+      "id": 3,
+      "name": "Bob",
+      "surname": "Johnson",
+      "email": "bob.johnson@example.com",
+      "tel": "555-9101"
+    }
+  ]
